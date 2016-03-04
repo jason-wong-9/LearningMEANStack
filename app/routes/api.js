810d58d76daeae17fc5a12401abf1dbc5f687cbd
Jason Wong <jason_wong_9@alumni.ubc.ca>
2016-03-04 23:20:17 +0000

    Token

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,7 +4,20 @@ var config = require('../../config');
 
 var secretKey = config.secretKey;
 
+var jsonwebtoken = require('jsonwebtoken');
 
+function createToken (user) {
+    
+    var token = jsonwebtoken.sign({
+       _id: user._id,
+       name: user.name,
+       username: user.username
+    }, secretKey, {
+        expiresInMinute: 1440
+    });
+    
+    return token;
+}
 
 module.exports = function(app, express) {
     
@@ -24,6 +37,44 @@ module.exports = function(app, express) {
           } 
           
           res.json({ message: 'User has been created!' });
+       });
+    });
+    api.get("/users", function(req, res){
+        User.find({}, function(err, users){
+            if(err){
+                res.send(err);
+                return;
+            }
+            res.json(users);
+        });         
+    });
+
+    api.post("/login", function(req, res){
+       User.findOne({ 
+           username: req.body.username
+       }).select('password').exec(function(err, user) {
+          if(err) throw err;
+          
+          if(!user) {
+              res.send({ message: "User doesn't exist!" });
+          } else {
+              var validPassword = user.comparePassword(req.body.password);
+              
+              if(!validPassword){
+                  res.send({ message: "Invalid Password" })
+              } else {
+                  // Create Token
+                  var token = createToken(user);
+                  
+                  res.json({
+                      success: true,
+                      message: "Successfully login!",
+                      token: token
+                  });
+              }
+          }
+          
+          
        });
     });
     
