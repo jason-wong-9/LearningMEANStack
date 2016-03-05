86fffbac43d47684172d0cf06c5ab0c458a177fe
Jason Wong <jason_wong_9@alumni.ubc.ca>
2016-03-05 00:03:26 +0000

    Finish Backend

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,5 +1,5 @@
 var User = require('../models/user');
-
+var Story = require('../models/story');
 var config = require('../../config');
 
 var secretKey = config.secretKey;
@@ -9,7 +9,7 @@ var jsonwebtoken = require('jsonwebtoken');
 function createToken (user) {
     
     var token = jsonwebtoken.sign({
-       _id: user._id,
+       id: user._id,
        name: user.name,
        username: user.username
     }, secretKey, {
@@ -98,11 +98,37 @@ module.exports = function(app, express) {
     });
     
     
-    // Provide a legitimate token
+    // Provided a legitimate token
     
-    api.get('/', function(req, res){
-       res.json("Hello World!"); 
+    api.route('/')
+        .post(function(req, res){
+            var story = new Story({
+                creator: req.decoded.id,
+                content: req.body.content,
+            });  
+            story.save(function(err){
+                if(err) {
+                    res.send(err);
+                    return;
+                }
+                res.json({ message: "New Story Created" });
+            })
+        })
+        
+        .get(function(req, res){
+            Story.find({ creator: req.decoded.id }, function(err, stories) {
+                if (err) {
+                    res.send(err);
+                    return;
+                }
+                res.json(stories);
+            });
+        });
+        
+    api.get('/me', function(req, res) {
+        res.json(req.decoded);
     });
+        
     
     return api;
 }
